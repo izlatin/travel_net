25adfc6a44b1022dc9c5f16c39c9b6d3dc4a0ddc
Mikhail Khromov <khromov05@gmail.com>
2022-05-15 16:12:36 +0300

    сделал список постов который потом будет юзаться как feed наверн

--- travelnet/publications/views.py
+++ travelnet/publications/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from publications.models import Publication
+
+
+class PublicationList(ListView):
+    model = Publication
+    template_name = 'publications/publication_list.html'
+    context_object_name = 'publications'
+
+    def get_queryset(self):
+        # если чел разлогинен или у него 0 подписок, кидаем популярные посты
+        # иначе кидаем посты из подписок
+        if not self.request.user.is_authenticated or self.request.user.follows.count() == 0:
+            return Publication.objects.popular_posts()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        # TODO: add form data here so that we can press like/dislike/add comments
+
+        return context
+
--- travelnet/travelnet/urls.py
+++ travelnet/travelnet/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
+    path('publications/', include('publications.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('homepage.urls')),
     path('about/', include('about.urls'))
