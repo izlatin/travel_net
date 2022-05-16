e2378dc19aa6c47fdea82edcb4d658321d002beb
Mikhail Khromov <khromov05@gmail.com>
2022-05-17 01:49:09 +0300

    поправил/добавил/переделал модельки для публикаций и рейтинга + view для объектов публикаций (без стилей!!)

--- travelnet/rating/models.py
+++ travelnet/rating/models.py
@@ -1,12 +1,27 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from publications.models import Publication
+from publications.models import Publication, AuthorMixin, DatetimeCreatedMixin, VisibleMixin
 
 
-class Like(models.Model):
+class CommentManager(models.Manager):
+    def popular_comments(self, publication: Publication, count: int):
+        return self.filter(publication=publication, visible=True).order_by('commentlike')[:count]
+
+
+class Comment(AuthorMixin, DatetimeCreatedMixin, VisibleMixin):
+    text = models.TextField(max_length=2000)
     publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)  # мб здесь не каскад?
+    # TODO: change responding_to from cascade to something else
+    responding_to = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    objects = CommentManager()
 
 
+class PublicationLike(AuthorMixin, DatetimeCreatedMixin):
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+
+
+class CommentLike(AuthorMixin, DatetimeCreatedMixin):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
 
