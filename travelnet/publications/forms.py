8437d0ea41556fafed43c6b9e2ecf62fed73748b
Ivan <izlatin@yandex.ru>
2022-05-21 20:33:48 +0300

    simpliest idea for form creation post

---
+++ travelnet/publications/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Publication
+
+
+class CreatePublicationForm(forms.ModelForm):
+    text = forms.CharField(label='Текст', widget=forms.Textarea)
+    class Meta:
+        model = Publication
+        fields = ('location',)
