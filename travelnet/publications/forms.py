bcd5f8db23493484517b787173921298e70e7101
Ivan <izlatin@yandex.ru>
2022-05-22 13:30:47 +0300

    some styles corrected after bootstrap connected

--- travelnet/publications/forms.py
+++ travelnet/publications/forms.py
@@ -5,6 +5,7 @@ from .models import Publication
 
 class CreatePublicationForm(forms.ModelForm):
     text = forms.CharField(label='Текст', widget=forms.Textarea)
+
     class Meta:
         model = Publication
         fields = ('location',)
