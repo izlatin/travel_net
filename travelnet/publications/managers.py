a63efe268db9b51560118cce3afbe3bf4f95b0ba
Mikhail Khromov <khromov05@gmail.com>
2022-05-23 20:45:00 +0300

    пофиксил работу карты после мерджа

--- travelnet/publications/managers.py
+++ travelnet/publications/managers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.db.models import Count
 
@@ -14,12 +16,15 @@ class PublicationManager(models.Manager):
     def get_queryset(self):
         return PublicationQueryset(model=self.model, using=self._db)
 
-    def popular_posts(self, post_count, datetime_created_after):
+    def popular_posts(self, post_count, datetime_created_after=(datetime.datetime.now() - datetime.timedelta(days=14))):
         return self.get_queryset().select_and_prefetch() \
                    .filter(visible=True, datetime_created__gt=datetime_created_after) \
                    .annotate(publicationlike_count=Count('publicationlike')) \
                    .order_by('-publicationlike_count', '-datetime_created')[:post_count]
 
     def user_feed(self, user, post_count):
-        return self.get_queryset().filter(visible=True, author__in=user.follows.all()) \
-                   .order_by('-datetime_created')[:post_count]
+        return self.filter(visible=True).prefetch_related('publicationlike_set').prefetch_related(
+            'publication_set').filter(author__in=user.follows.all()).order_by('-datetime_created')[:post_count]
+
+    def maps_posts(self):
+        return self.select_related('location').filter(visible=True)
